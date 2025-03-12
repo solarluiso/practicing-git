8e5c0edcd64b5752aec79e10beb153a7b4d0fbfb
Luis <solarluiso@gmail.com>
2025-03-12 08:26:46 -0400

    remove PentagramForm component from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import PentagramForm from "./components/PentagramForm";
-
 function App() {
   return (
     <div className="w-full h-[100vh] flex items-center justify-center bg-gray-700">
       <h1 className="text-cyan-950 text-4xl">
-        Lets' get started with Tailwind CSS
-        <PentagramForm />
+        Lets' get started with Tailwind CSS        
       </h1>
     </div>
   );
